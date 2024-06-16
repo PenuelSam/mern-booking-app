cafe4ce00d88ba09de1161d429a36ee0475d6f95
PenuelSam <penuelsamuel54@gmail.com>
2024-06-16 21:53:22 +0100

    added my hotels view and added e2e test

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -1,7 +1,8 @@
 import express,{Request, Response} from "express";
 import multer from 'multer'
 import cloudinary from 'cloudinary'
-import Hotel, { HotelType } from "../models/hotel";
+import Hotel from "../models/hotel";
+import { HotelType } from "../shared/types";
 import verifyToken from "../middleware/auth";
 import { body } from "express-validator";
 
@@ -15,7 +16,7 @@ const upload = multer({
     }
 })
 
-// api/my-hotels
+
 router.post(
     "/", 
 verifyToken , [
@@ -60,5 +61,18 @@ async (req: Request, res: Response) => {
         res.status(500).json({message: "something went wrong"})
     }
 });
+
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+   
+
+    try {
+
+        const hotels = await Hotel.find({userId: req.userId});
+        res.json(hotels);
+        
+    } catch (error) {
+        res.status(500).json({message: "Error fetching hotels"})
+    }
+})
 
 export default router;
